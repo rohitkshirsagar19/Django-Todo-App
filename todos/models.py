02df3c2c371c41ae4fd47c30e11752c7608d442c
rohitkshirsagar19 <rohitkshirsagar1904@gmail.com>
2025-08-30 23:54:49 +0530

    FEAT: Added todos app into backend

---
+++ todos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=25,choices=STATUS_CHOICES, default='PENDING')
+    priority = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
